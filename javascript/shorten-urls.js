fdb5de425e39e2d2f4641d22b812188332f30b38
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2024-08-15 14:23:08 +0530

    Update shorten-urls.js

--- javascript/shorten-urls.js
+++ javascript/shorten-urls.js
@@ -1,36 +1,31 @@
-document.addEventListener('DOMContentLoaded', async function() {
-    async function shortenUrl(url) {
-        const apiToken = '17950084501a0650a1be53be2dc8437fa202f0ce';
-        const encodedUrl = encodeURIComponent(url);
-        const apiUrl = `https://modijiurl.com/api?api=${apiToken}&url=${encodedUrl}`;
-        
-        try {
-            const response = await fetch(apiUrl);
-            const result = await response.json();
-            if (result.status === 'success') {
-                return result.shortenedUrl;
-            } else {
-                console.error('Error shortening URL:', result.message);
-                return url;
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            return url;
-        }
-    }
+// shorten-urls.js
 
-    const links = document.querySelectorAll('a');
-    for (const link of links) {
-        const originalUrl = link.href;
+var app_url = 'https://modijiurl.com/';
+var app_api_token = '17950084501a0650a1be53be2dc8437fa202f0ce';
+var app_advert = 2;
+var app_exclude_domains = ["Google.com"];
 
-        if (window.location.pathname.includes('/posts/')) {
-            // Check if the current page is within the posts folder
-            try {
-                const shortenedUrl = await shortenUrl(originalUrl);
-                link.href = shortenedUrl;
-            } catch (error) {
-                console.error('Failed to shorten URL for', originalUrl, error);
-            }
-        }
+function shortenAndRedirect(event) {
+    event.preventDefault(); // Prevent the default action
+
+    var originalUrl = event.currentTarget.href;
+    var excludeDomains = app_exclude_domains.map(domain => domain.toLowerCase());
+
+    // Check if the original URL's domain is in the exclude list
+    var domain = new URL(originalUrl).hostname.toLowerCase();
+    if (excludeDomains.includes(domain)) {
+        window.location.href = originalUrl; // Directly go to the original URL
+    } else {
+        // Construct the shortener URL with parameters
+        var shortenedUrl = app_url + 'api?api=' + app_api_token + '&url=' + encodeURIComponent(originalUrl) + '&advert=' + app_advert;
+        window.location.href = shortenedUrl; // Redirect to the shortened URL
     }
+}
+
+// Attach the event listener to all anchor tags once the page loads
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.querySelectorAll('a');
+    links.forEach(function (link) {
+        link.addEventListener('click', shortenAndRedirect);
+    });
 });
