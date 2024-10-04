c5181c5892d8a4fe0ad711899507d16c2d0010a3
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2024-10-04 10:20:26 +0530

    Update Tutorial.js

--- javascript/Tutorial.js
+++ javascript/Tutorial.js
@@ -14,7 +14,7 @@ openTutorialModelLink.addEventListener('click', () => {
     // After 3 seconds, inject the iframe with autoplay enabled
     setTimeout(() => {
         videoContainer.innerHTML = `
-            <iframe width="560" height="315" 
+            <iframe width="426" height="240" 
                 src="https://www.youtube.com/embed/qRa8WCkigmk?autoplay=1" 
                 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                 allowfullscreen></iframe>
